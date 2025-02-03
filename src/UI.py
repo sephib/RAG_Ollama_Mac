fe6c8f0e28ea8b26aea2459914687897360fce2f
pruthvirajcyn <57512666+pruthvirajcyn@users.noreply.github.com>
2025-02-02 22:53:52 -0500

    Development of a RAG Chatbot
    This project focuses on building a Retrieval-Augmented Generation (RAG) chatbot that combines the power of large language models (LLMs) with external knowledge retrieval for accurate and context-aware responses. The chatbot leverages vector databases for efficient document retrieval and LangChain for seamless integration of LLMs, enabling it to answer user queries by dynamically fetching and synthesizing relevant information. Key features include real-time data access, scalable knowledge integration, and customizable prompts for domain-specific use cases. The repository includes code, documentation, and examples for deploying the chatbot in various applications, such as customer support, document Q&A, and personalized assistance.

---
+++ src/UI.py
@@ -0,0 +1,37 @@
+from rag_query import query_rag
+import streamlit as st
+
+    
+st.set_page_config(page_title="FactBot: No Hallucinations, Just Facts")
+with st.sidebar:
+    st.title('FactBot')
+
+# Function for generating LLM response
+def generate_response(input):
+    result = query_rag(input)
+    return result
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome, get ready to be mind blown by AI"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# User-provided prompt
+if input := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": input})
+    with st.chat_message("user"):
+        st.write(input)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Getting your answer from superior intelligence.."):
+            response = generate_response(input) 
+            
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
---
+++ src/rag_query.py
@@ -0,0 +1,59 @@
+import argparse
+from langchain.vectorstores.chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+from embedding import get_embedding_function
+import re
+
+CHROMA_PATH = "chroma"
+
+PROMPT= """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context in a brief: {question}
+"""
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag(query_text)
+
+
+
+def remove_think_tags(response):
+    return re.sub(r'<think>.*?</think>', '', response, flags=re.DOTALL)
+
+def query_rag(query: str):
+    # Prepare the DB.
+    embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_score(query, k=5)
+
+    context = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT)
+    prompt = prompt_template.format(context=context, question=query)
+    # print(prompt)
+
+    #model = Ollama(model="mistral")
+    model = Ollama(model="deepseek-r1")
+    #model = Ollama(model="llama3")
+    response_text = model.invoke(prompt)
+    response = remove_think_tags(response_text)
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+    return response
+
+
+if __name__ == "__main__":
+    main()
